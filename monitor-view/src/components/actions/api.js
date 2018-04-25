909db64099392afae3a892ebd11d534ce48e3df0
Jordan Marshall <jordan.marshall@acadiahealthcare.com>
2018-04-25 16:23:01 -0500

    api for loading in domain status and values from request, css updates

--- monitor-view/src/components/actions/api.js
+++ monitor-view/src/components/actions/api.js
@@ -1,37 +1,42 @@
 import axios from "axios";  // request library
 
-// url for GOAuth
-var oauthURL = "http://localhost:5050/oauth";
+// url for monitor status info endpoint
+var statusURL = "http://localhost:5000/getDomainInfo";
 
 // Init form data
-var fData = new FormData();
+// var fData = new FormData();
 
 export default {
-    user: {
-        login: credentials => {
-            // set form data
-            fData.set('user', credentials.user);
-            fData.set('pass', credentials.pass);
-            // axios post request with credentials, send with form-data
-            return axios({
-                method: 'post',
-                url: oauthURL,
-                data: fData,
-                config: { headers: {'Content-Type': 'application/x-www-form-urlencoded' }}
-            })
-            .then((response) => {
-                // on success: return response data from server (email, token)
-                return response.data;
-            })
-            // .catch(function (error) {
-            //     console.log(error);
-            // })
-        }
-    },
+    // user: {
+    //     login: credentials => {
+    //         // set form data
+    //         fData.set('user', credentials.user);
+    //         fData.set('pass', credentials.pass);
+    //         // axios post request with credentials, send with form-data
+    //         return axios({
+    //             method: 'post',
+    //             url: oauthURL,
+    //             data: fData,
+    //             config: { headers: {'Content-Type': 'application/x-www-form-urlencoded' }}
+    //         })
+    //         .then((response) => {
+    //             // on success: return response data from server (email, token)
+    //             return response.data;
+    //         })
+    //         // .catch(function (error) {
+    //         //     console.log(error);
+    //         // })
+    //     }
+    // },
 
     status: {
-        getStatusInfo: creds => {
-            
+        getStatusInfo: () => {
+            return axios.get(statusURL).then((resp) => {
+                return resp.data;
+            })
+                .catch(function (err) {
+                    console.log(err);
+                })
         }
     }
 }
