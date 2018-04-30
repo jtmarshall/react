419c93c131d518f870b0688c0f3b48fb2b3a835c
Jordan <jordantmarshall3@gmail.com>
2018-04-30 11:14:12 -0500

    remove old api code from test project

--- monitor-view/src/components/actions/api.js
+++ monitor-view/src/components/actions/api.js
@@ -7,28 +7,6 @@ var statusURL = "http://go-monitor.us-east-1.elasticbeanstalk.com/getDomainInfo"
 // var fData = new FormData();
 
 export default {
-    // user: {
-    //     login: credentials => {
-    //         // set form data
-    //         fData.set('user', credentials.user);
-    //         fData.set('pass', credentials.pass);
-    //         // axios post request with credentials, send with form-data
-    //         return axios({
-    //             method: 'post',
-    //             url: oauthURL,
-    //             data: fData,
-    //             config: { headers: {'Content-Type': 'application/x-www-form-urlencoded' }}
-    //         })
-    //         .then((response) => {
-    //             // on success: return response data from server (email, token)
-    //             return response.data;
-    //         })
-    //         // .catch(function (error) {
-    //         //     console.log(error);
-    //         // })
-    //     }
-    // },
-
     status: {
         getStatusInfo: () => {
             return axios.get(statusURL).then((resp) => {
