7f57ac0e0901c07592dc67eac211e132addf54e3
Jordan <jordantmarshall3@gmail.com>
2018-05-08 12:03:52 -0500

    finalize weekly and monthly data views

--- monitor-view/src/components/actions/api.js
+++ monitor-view/src/components/actions/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";  // request library
 
 // url for monitor status info endpoint
-//const statusURL = "http://go-monitor.us-east-1.elasticbeanstalk.com/getDomainInfo";
 const statusURL = "http://go-monitor.us-east-1.elasticbeanstalk.com/monitorstatus";
 const monthlyStatusURL = "http://go-monitor.us-east-1.elasticbeanstalk.com/monthlymonitorstatus";
 
